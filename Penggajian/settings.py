00deb8a875e026730e7b7d8f215644138a43cc2a
Dera Akbar <104042969+deraakubaru@users.noreply.github.com>
2023-09-01 14:35:42 +0700

    Update settings.py

--- Penggajian/settings.py
+++ Penggajian/settings.py
@@ -78,13 +78,9 @@ WSGI_APPLICATION = 'Penggajian.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
-DATABASES['default'] = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
